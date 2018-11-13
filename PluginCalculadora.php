d1bcebceee16ebc1a01c527318ac9a1cd43b4507
Paulo Cichetto <paulocichetto@gmail.com>
2018-11-13 17:17:10 +0000

    Calculadora js

---
+++ PluginCalculadora.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Plugin Name: plugin_calculadora
+* Description: plugin de exemplo
+* Version: 1.0
+* Author: Paulo Aquino
+*/
+
+//principal função usada no plugin
+function Calculo( $atts ){
+    return '<iframe src="'.plugins_url().'/PluginCalculadora/index.php" frameborder="0" width="400px" height="500px"></iframe>';
+}
+add_shortcode('calculadora', 'Calculo');
+?>
