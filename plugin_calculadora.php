106d76ea97ea667960371b7a22b465c71716778e
Stefany <stefany22lisboa@gmail.com>
2018-11-20 16:32:25 +0000

    estilização

--- plugin_calculadora.php
+++ plugin_calculadora.php
@@ -9,7 +9,7 @@
 
 //principal função usada no plugin
 function Calculo( $atts ){
-    return '<iframe src="'.plugins_url().'/PluginCalculadora/index.php" frameborder="0" width="400px" height="500px"></iframe>';
+    return '<iframe src="'.plugins_url().'/plugin_calculadora/index.php" frameborder="0" width="400px" height="500px"></iframe>';
 }
 add_shortcode('calculadora', 'Calculo');
 ?>
